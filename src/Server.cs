faf02f8ed959102adaa32867dc2d64318718f63d
André Minelli <andreminelli@gmail.com>
2025-04-05 11:41:57 -0300

    Add dummy request receiving and hard-coded 200 response

--- src/Server.cs
+++ src/Server.cs
@@ -1,6 +1,15 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
-server.AcceptSocket(); // wait for client
+var clientSocket = await server.AcceptSocketAsync(); // wait for client
+
+var requestBuffer = new byte[2048];
+var request = await clientSocket.ReceiveAsync(new ArraySegment<byte>(requestBuffer), SocketFlags.None);
+// TODO Parse the request
+
+byte[] responseBytes = Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n");
+await clientSocket.SendAsync(new ArraySegment<byte>(responseBytes), SocketFlags.None);
+clientSocket.Close();
