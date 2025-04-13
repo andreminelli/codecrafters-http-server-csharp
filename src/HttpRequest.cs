04f588e3acecb5e9efd98a7a06c7d00447973135
André Minelli <andreminelli@gmail.com>
2025-04-13 16:33:27 -0300

    Implement request headers parsing

--- src/HttpRequest.cs
+++ src/HttpRequest.cs
@@ -4,15 +4,24 @@ namespace codecrafters_http_server.src;
 
 public record HttpRequest(string Method, string Path, string Version, ReadOnlyDictionary<string, string> Headers, string Body)
 {
+    private const string LineBreak = "\r\n";
+
     public static HttpRequest Parse(string request)
     {
-        var lines = request.Split(["\r\n"], StringSplitOptions.None);
+        var lines = request.Split([LineBreak], StringSplitOptions.None);
 
         var (method, path, version) = ParseRequestLine(lines[0]);
 
         var headers = new Dictionary<string, string>();
+        var linePosition = 1;
+        for (var i = linePosition; i < lines.Length; i++, linePosition++)
+        {
+            if (string.IsNullOrWhiteSpace(lines[i])) break;
 
-        // TODO Parse headers
+            var headerParts = lines[i].Split(':');
+            headers.Add(headerParts[0], string.Join(':', headerParts.Skip(1)));
+        }
+
         // TODO Parse body
 
         return new HttpRequest(method, path, version, new ReadOnlyDictionary<string, string>(headers), string.Empty);
